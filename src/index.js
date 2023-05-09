08f6dc56b8f35750688e4d2917c2b67097b62574
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2023-05-09 17:18:45 -0300

    Create basic functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,49 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response)=> {
-    return response.json({message: "fundamentos node js"})
+app.get("/courses", (request, response)=> {
+
+    return response.json([
+        "Curso 1:",
+        "Curso 2:",
+        "Curso 3:",
+        "Curso 4:"]);
+    
 });
+
+app.post("/courses", (request, response) => {
+    return response.json([
+        "Curso 1:",
+        "Curso 2:",
+        "Curso 3:",
+        "Curso 4:",
+        "Curso 5:"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6:",
+        "Curso 2:",
+        "Curso 3:",
+        "Curso 4:",
+        "Curso 5:"])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6:",
+        "Curso 7:",
+        "Curso 3:",
+        "Curso 4:",
+        "Curso 5:"])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6:",
+        "Curso 2:",
+        "Curso 4:",
+        "Curso 5:"])
+})
 
 app.listen(3003);
